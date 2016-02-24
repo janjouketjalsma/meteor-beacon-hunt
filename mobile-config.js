6c93a8108c70688ba0d2b52b6671a09b0039339d
Ward Gubbi <ward.gubbi@23seconds.be>
2016-02-24 16:52:44 +0000

    Add mobile config, add bootstrap, basic design elements (beacons & pulsing radar)

---
+++ mobile-config.js
@@ -0,0 +1,8 @@
+App.info({
+  id: 'com.mff.beaconhunt',
+  name: 'MFFBeaconHunt',
+  description: 'Hunt down the Beacon',
+  author: 'Meteor in Flip Flops',
+  email: 'contact@example.com',
+  website: 'https://www.meteorinflipflops.com'
+});
