7c20308fe6e290f4a7ae8f0b1c4fe38ef7f76460
neohipper <neohipper@gmail.com>
2016-02-24 12:04:08 +0100

    Added some files to put beacon test code in it

---
+++ beaconTest.js
@@ -0,0 +1,9 @@
+/*
+if(Meteor.isCordova){
+  const Big     = new ReactiveBeaconRegion({identifier: "Big", uuid: "B9407F30-F5F8-466E-AFF9-25556B57FE6D"});
+  const Bed     = new ReactiveBeaconRegion({identifier: "Bed", uuid: "D0D3FA86-CA76-45EC-9BD9-6AF428C8C61F"});
+  const Fridge  = new ReactiveBeaconRegion({identifier: "Fridge", uuid: "D0D3FA86-CA76-45EC-9BD9-6AF4EB014D04"});
+  const Dog     = new ReactiveBeaconRegion({identifier: "Dog", uuid: "D0D3FA86-CA76-45EC-9BD9-6AF4BBEBADDD"});
+
+  let BigInfo = reactiveBeaconRegion.getBeaconRegion();
+});*/
